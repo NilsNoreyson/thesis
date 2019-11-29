b4f57efbfd91bf89cdd65ac0f91549096619eb0e
Peter Stüber <peter.stueber@krypton.ipc.uni-tuebingen.de>
2019-11-29 16:52:54 +0100

    on home

---
+++ make_pdfs.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+files=(1_Introduction.ipynb 2-Grain-SMOX.ipynb 3-Resistance-sensor.ipynb 4-Temperature-Sensor.ipynb)
+outdir=latex
+
+
+cp -r media latex/
+cp -r ipython.bib latex/
+
+for f in ${files[*]};
+do
+//jupyter-nbconvert --to latex --output-dir $outdir $f
+done
+
+cd $outdir
+
+for i in {1..3};
+do
+	for f in ${files[*]};
+	do
+		//pdflatex "$(basename "$f" .ipynb).tex"
+	done
+echo $i
+done
+
+
+for f in ${files[*]};
+do
+	bibtex "$(basename "$f" .ipynb).aux"
+done
+
+for f in ${files[*]};
+do
+	pdflatex   "$(basename "$f" .ipynb).tex"
+done
+
+for f in ${files[*]};
+do
+	cp "$(basename "$f" .ipynb).pdf" "../PDFs"
+done
+
+
